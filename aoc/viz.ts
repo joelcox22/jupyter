4167b2bd1b9f3aded09850e597d938a58fd22b01
Joel Cox <joel@pinecodes.com>
2024-12-04 21:27:53 +0800

    test

--- aoc/viz.ts
+++ aoc/viz.ts
@@ -32,11 +32,13 @@ export class Viz {
     const outfile = path.join(this.tmpDir, 'output.mp4');
     await $`/opt/homebrew/bin/ffmpeg -framerate ${fps} -pattern_type glob -i ${path.join(this.tmpDir, '*.png')} -r ${fps} -c:v libx264 -pix_fmt yuv420p ${outfile}`;
     const out = fs.readFileSync(outfile);
-    Deno.jupyter.display(Deno.jupyter.md`<video controls><source src="data:video/mp4;base64,${out.toString('base64')}"></video>`);
+    Deno.jupyter.display(Deno.jupyter.html`<video controls><source src="data:video/mp4;base64,${out.toString('base64')}"></video>`);
     if (filename) {
       console.log('copying', outfile, 'to', filename)
       fs.cpSync(outfile, filename);
-      Deno.jupyter.display(Deno.jupyter.md`![video](./${filename})`)
+      Deno.jupyter.display(Deno.jupyter.md`![video](./${filename})
+        
+https://github.com/user-attachments/assets/d82f9cc6-0d68-4fdb-aad1-2e66b0dc97e9`)
     }
   }
 }
